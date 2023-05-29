1d612ea7dadcf16ed01c18303288e66c95d6e55b
jenniekibiri <josphatje@gmail.com>
2023-05-29 15:03:21 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// const transporter = createTransport({
+//     host: 'host.docker.internal',
+//     port: 1025
+// });
+
+// await transporter.sendMail({
+//     from: 'from@example.com',
+//     to: 'admin@admin.com',
+//     subject: 'An order has been completed',
+//     html: `Order #${order.id} with a total of $${order.total} has been completed`
+// });
+
+// await transporter.sendMail({
+//     from: 'from@example.com',
+//     to: order.ambassador_email,
+//     subject: 'An order has been completed',
+//     html: `You earned $${order.ambassador_revenue} from the link #${order.code}`
+// });
+
+// await transporter.close();
