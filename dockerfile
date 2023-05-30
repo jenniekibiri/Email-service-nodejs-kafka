5b4469645315cfd1bb6b1dbf4417fc410a0dc24e
jenniekibiri <josphatje@gmail.com>
2023-05-30 18:27:36 +0300

    email service nodejs kafka

---
+++ dockerfile
@@ -0,0 +1,7 @@
+From node:17
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+
+CMD ["npm", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,50 @@
-// const transporter = createTransport({
-//     host: 'host.docker.internal',
-//     port: 1025
-// });
+import { Kafka, EachMessagePayload } from "kafkajs";
+import { createTransport } from "nodemailer";
+const kafka = new Kafka({
+  clientId: "email-consumer",
+  brokers: ["pkc-6ojv2.us-west4.gcp.confluent.cloud:9092"],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: "BLM3TQS2LD7RBGRF",
+    password:
+      "ye+HC2dt2tyVeTfmW10GaxrATsKlX+mjJiZBMaYz42Njqk/CilL+b4K2a74XxTaH",
+  },
+  connectionTimeout: 3000,
+  enforceRequestTimeout: true,
+});
+const transporter = createTransport({
 
-// await transporter.sendMail({
-//     from: 'from@example.com',
-//     to: 'admin@admin.com',
-//     subject: 'An order has been completed',
-//     html: `Order #${order.id} with a total of $${order.total} has been completed`
-// });
+  host: "mailhog",
+  port: 1025,
 
-// await transporter.sendMail({
-//     from: 'from@example.com',
-//     to: order.ambassador_email,
-//     subject: 'An order has been completed',
-//     html: `You earned $${order.ambassador_revenue} from the link #${order.code}`
-// });
+});
 
-// await transporter.close();
+const consumer = kafka.consumer({ groupId: "email-consumer" });
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "default" });
+  await consumer.run({
+    eachMessage: async (message: EachMessagePayload) => {
+      console.log("Email sent", message.message,"test");
+      // const order = JSON.parse(message.message.value.toString());
+      await transporter.sendMail({
+        from: "from@example.com",
+        to: "jennykibiri@gmail.com",
+        subject: "An order has been completed",
+        html: `Order #{order.id} with a total of {order.id} has been completed`,
+      });
+
+      await transporter.sendMail({
+        from: "from@example.com",
+        to: "from@example.com",
+        subject: "An order has been completed",
+        html: `You earned {order.ambassador_revenue} from the link #{order.code}`,
+      });
+    },
+  });
+  transporter.close();
+};
+
+run().then(() => console.error);
